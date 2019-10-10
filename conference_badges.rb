53c85e4ad85a3a7e4cbdb9e94535bda9e1215012
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-10 14:50:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,7 @@ def assign_rooms(attendees)
   count = 1
   
   attendees.each do |x|
-    puts "Hello, _____! You'll be assigned to room _____!"
+    puts "Hello, #{x}! You'll be assigned to room #{count}!"
+    count += 1
   end
 end
