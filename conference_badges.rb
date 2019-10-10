fcc1138eafb2c448d5c337e64afc230ab5b28303
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-10 14:38:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |x|
+    attendees[attendees.index(x)] = "Hello, my name is #{name}."
+  end
+end
