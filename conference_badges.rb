98a423c2132ba5e2eb8fb729ebec9df385256b25
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-10 14:48:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,13 +5,15 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.collect do |name|
-    badge_maker(name)
+  attendees.collect do |x|
+    badge_maker(x)
   end
 end
 
-def assign_rooms(speakers)
+def assign_rooms(attendees)
   count = 0
   
-  
+  attendees.each do |x|
+    
+  end
 end
